8998ff20b4c46b1c2e5129432c4fc4a4c3911015
camenduru <54370274+camenduru@users.noreply.github.com>
2023-09-11 19:10:07 +0300

    test

--- demo.py
+++ demo.py
@@ -14,6 +14,8 @@ volume = modal.NetworkFileSystem.persisted("DiffBIR")
         apt install -y git git-lfs && \
         git --version  && \
         apt install -y aria2 libgl1 libglib2.0-0 wget && \
+        wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && \
+        dpkg -i cloudflared-linux-amd64.deb && \
         pip install -q torch==2.0.1+cu118 torchvision==0.15.2+cu118 torchaudio==2.0.2+cu118 torchtext==0.15.2 torchdata==0.6.1 --extra-index-url https://download.pytorch.org/whl/cu118 && \
         pip install -q xformers==0.0.20 triton==2.0.0 && \
         pip install -q einops pytorch_lightning gradio omegaconf transformers lpips opencv-python && \
@@ -24,6 +26,31 @@ volume = modal.NetworkFileSystem.persisted("DiffBIR")
     timeout=60000,
 )
 async def run():
+    import atexit, requests, subprocess, time, re
+    from random import randint
+    from threading import Timer
+    from queue import Queue
+    def cloudflared(port, metrics_port, output_queue):
+        atexit.register(lambda p: p.terminate(), subprocess.Popen(['cloudflared', 'tunnel', '--url', f'http://127.0.0.1:{port}', '--metrics', f'127.0.0.1:{metrics_port}'], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT))
+        attempts, tunnel_url = 0, None
+        while attempts < 10 and not tunnel_url:
+            attempts += 1
+            time.sleep(3)
+            try:
+                tunnel_url = re.search("(?P<url>https?:\/\/[^\s]+.trycloudflare.com)", requests.get(f'http://127.0.0.1:{metrics_port}/metrics').text).group("url")
+            except:
+                pass
+        if not tunnel_url:
+            raise Exception("Can't connect to Cloudflare Edge")
+        output_queue.put(tunnel_url)
+    output_queue, metrics_port = Queue(), randint(8100, 9000)
+    thread = Timer(2, cloudflared, args=(7860, metrics_port, output_queue))
+    thread.start()
+    thread.join()
+    tunnel_url = output_queue.get()
+    os.environ['webui_url'] = tunnel_url
+    print(tunnel_url)
+
     os.environ['HF_HOME'] = '/content/cache/huggingface'
     os.system(f"git clone -b dev https://github.com/camenduru/DiffBIR /content/DiffBIR")
     os.chdir(f"/content/DiffBIR")
